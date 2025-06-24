f5a8f423ed53c2da122f512f8d26e0bf40875c85
jtan0435 <jtan0435@student.monash.edu>
2025-06-24 16:11:01 +0800

    Removed readability as it removes a large portion of links.

--- indexing/url_details_scraper.py
+++ indexing/url_details_scraper.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup, Comment
 import os
 import asyncio
 import aiofiles
-from readability import Document
+import time
+
+start_time = time.time()
 
 # Load all links from the CSV
 links = []
@@ -17,8 +19,12 @@ with open("links.csv", newline='', encoding="utf-8") as f:
 os.makedirs("pages", exist_ok=True)
 
 async def scrape_page(browser, title, url):
-    page = await browser.new_page()
-    print(f"Scraping: {title} - {url}")
+    
+    context = await browser.new_context()
+    page = await context.new_page()
+    current_index = links.index((title, url)) + 1
+    total_links = len(links)
+    print(f"Scraping ({current_index}/{total_links}): {title} - {url}")
 
     # For simplicity, we will disregard pdf files first
     if url.lower().endswith(".pdf"):
@@ -37,36 +43,32 @@ async def scrape_page(browser, title, url):
     except:
         pass
 
-    try:
-        # Use Readability to extract key information
-        response = Document(await page.content())
-    except:
+    # Resort to manual scraping
+    html = await page.content()
+    soup = BeautifulSoup(html, "html.parser")
 
-        # Resort to manual scraping
-        html = await page.content()
-        soup = BeautifulSoup(html, "html.parser")
+    raw_html = soup.find("div", class_="content-inner__main")
+    if not raw_html:
+        await page.close()
+        return
 
-        raw_html = soup.find("div", class_="content-inner__main")
-        if not raw_html:
-            await page.close()
-            return
+    for tag in raw_html.find_all(["nav", "style", "script", "noscript", "iframe"]):
+        tag.decompose()
 
-        for tag in raw_html.find_all(["nav", "style", "script", "noscript", "iframe"]):
-            tag.decompose()
+    faq_help_panel = raw_html.find("div", id="monash-faq-help-panel")
+    if faq_help_panel:
+        faq_help_panel.decompose()
 
-        faq_help_panel = raw_html.find("div", id="monash-faq-help-panel")
-        if faq_help_panel:
-            faq_help_panel.decompose()
+    for comment in raw_html.find_all(string=lambda text: isinstance(text, Comment)):
+        comment.extract()
 
-        for comment in raw_html.find_all(string=lambda text: isinstance(text, Comment)):
-            comment.extract()
-
-        response = str(raw_html)
+    response = str(raw_html)
 
     async with aiofiles.open(f"pages/{title}.txt", "w", encoding="utf-8") as out:
-        await out.write(response.summary())
+        await out.write(response)
 
     await page.close()
+    await context.close()
 
 async def main():
     async with Stealth().use_async(async_playwright()) as p:
@@ -82,5 +84,8 @@ async def main():
         tasks = [sem_scrape(title, url) for title, url in links]
         await asyncio.gather(*tasks)
         await browser.close()
+
+        end_time = time.time()
+        print(f"Scraper took {end_time - start_time:.2f} seconds to scrape all URL's information.")
 
 asyncio.run(main())
